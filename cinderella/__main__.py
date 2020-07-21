15297dd1623ec5ab878dbfd8500daacf414e25bb
VWO53 <64356181+VWO53@users.noreply.github.com>
2020-07-21 15:50:22 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -179,8 +179,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="Add Me🎉", url="t.me/{}?startgroup=true".format(bot.username),InlineKeyboardButton(text="Creator👲",url="https://t.me/Lns4n3")]]
-    keyboard += [[InlineKeyboardButton(text="Connect Group📌", callback_data="main_connect"),InlineKeyboardButton(text="Help❔",callback_data="help_back")]]
+    keyboard = [[InlineKeyboardButton(text="Add Me To Group🎉", url="t.me/{}?startgroup=true".format(bot.username)]]
+    keyboard += [[InlineKeyboardButton(text="Connect Group📌",callback_data="main_connect"),InlineKeyboardButton(text="Help❔",callback_data="help_back")]]
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
